d52e2e5a83d4e3971978c28391d1cd1c3b585a33
aaron.harpt <aaron.harpt@gmail.com>
2016-01-07 10:54:51 -0600

    added gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+'use strict'
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+
+gulp.task("hello", function(){
+	console.log("hello");
+});
+
+gulp.task("compileSass", function(){
+gulp.src("src/main.scss")
+.pipe(sass())
+.pipe(gulp.dest("css/main.css"));
+});
+
+gulp.task("default", ["hello", "compileSass"], function(){
+	console.log("default task");
+});
