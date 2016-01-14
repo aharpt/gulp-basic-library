ae1790ae6a6e090d1ce4731780914177967b70ad
aaron.harpt <aaron.harpt@gmail.com>
2016-01-14 15:06:58 -0600

    changed watchSass task to watch in build task

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,7 @@ gulp.src("src/main.scss")
 .pipe(gulp.dest("css"));
 });
 		
-gulp.task("watchFiles", function(){
+gulp.task("watch", function(){
 	gulp.watch("src/*.scss", ["compileSass"]);
 	gulp.watch("js/production.js", ["concatScripts"]);
 });
@@ -44,11 +44,11 @@ gulp.task("clean", function() {
 	clean(["dist", "css/main.css*", "js/production.js*", "production/production.min.js"]);
 })
 
-gulp.task("build", ["hello", "concatScripts", "minifyScripts", "compileSass", "watchSass"], function() {
+gulp.task("build", ["hello", "concatScripts", "minifyScripts", "compileSass", "watch"], function() {
 	return gulp.src(["css/main.css", "production/production.min.js", "index.html", "img/**", "fonts/**"], {base: "./"})
 	.pipe(gulp.dest("dist"));
 });
-
+	
 gulp.task("default", ["clean"], function() {
 	gulp.start("build");
 });
