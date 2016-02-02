eeb3c0f1e936bf4f866ab5b6ba894a34cdab6bd2
aaron.harpt <aaron.harpt@gmail.com>
2016-02-02 17:08:45 -0600

    removed gulp load plugin options

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,13 @@
 
 // requiring gulp and gulp plugins
 var gulp = require("gulp");
-var plugins = require("gulp-load-plugins")({
-	lazy: false, pattern:"*"
-});
+var plugins = require("gulp-load-plugins")();
 var clean = require("del");	
 		
 // Concat javascript and put into a file named production.js in the js folder
 gulp.task('concatScripts', function() {
 	return gulp.src('src/*.js')	
-	  .pipe(plugins.maps.init())
+	  .pipe(plugins.sourcemaps.init())
 .pipe(plugins.concat('production.js'))
   .pipe(plugins.sourcemaps.write("./"))
 .pipe(gulp.dest('js'));
@@ -25,10 +23,10 @@ gulp.task("minifyScripts", ["concatScripts"], function(){
 
 gulp.task("compileSass", function(){
 gulp.src("src/main.scss")
-.pipe(plugins.maps.init())
+.pipe(plugins.sourcemaps.init())
 .pipe(plugins.autoprefixer())
 .pipe(plugins.sass())		
-.pipe(plugins.maps.write("./"))
+.pipe(plugins.sourcemaps.write("./"))
 .pipe(gulp.dest("css"));	
 });
 		
