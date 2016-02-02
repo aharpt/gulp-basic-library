f409ead4a765f6ae4ab924a719dc85940638ae05
aaron.harpt <aaron.harpt@gmail.com>
2016-02-02 16:46:33 -0600

    removed unneeded required gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,29 @@
-"use strict";
+// "use strict";
 
 var gulp = require("gulp");
 var plugins = require("gulp-load-plugins")({
 	lazy: false, pattern:"*"
 });
-var pluginsgulpConcat = require("gulp-concat");
-var pluginsgulpMinify = require("gulp-uglify");
-var pluginssass = require("gulp-sass");
-var pluginsrename = require("gulp-rename");
-var pluginssourcemaps = require("gulp-sourcemaps");		
-var pluginsclean = require("del");	
-var pluginsautoprefixer = require("gulp-autoprefixer");
+// var pluginsgulpConcat = require("gulp-concat");
+// var pluginsgulpMinify = require("gulp-uglify");
+// var pluginssass = require("gulp-sass");
+// var pluginsrename = require("gulp-rename");
+// var pluginssourcemaps = require("gulp-sourcemaps");		
+var clean = require("del");	
+// var pluginsautoprefixer = require("gulp-autoprefixer");
 		
 
 gulp.task('concatScripts', function() {
 	return gulp.src('src/*.js')	
 	  .pipe(plugins.sourcemaps.init())
-.pipe(pluginsgulpConcat('production.js'))
+.pipe(plugins.concat('production.js'))
   .pipe(plugins.sourcemaps.write("./"))
 .pipe(gulp.dest('js'));
 });
 
 gulp.task("minifyScripts", ["concatScripts"], function(){
 	return gulp.src('js/production.js')
-	.pipe(pluginsgulpMinify())	
+	.pipe(plugins.uglify())	
 	 .pipe(gulp.dest("js"));		
 });
 
@@ -42,7 +42,7 @@ gulp.task("watch", function(){
 });
 
 gulp.task("clean", function() {
-	pluginsclean(["dist", "css/main.css", "js/production.js*", "production/production.min.js"]);
+	clean(["dist", "css/main.css", "js/production.js*", "production/production.min.js"]);
 });
 
  gulp.task("build", [ "concatScripts", "minifyScripts", "compileSass", "watch"], function() {
