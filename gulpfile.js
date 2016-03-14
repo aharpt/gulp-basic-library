c9bfe5593766957e095febe37e648acf558a8a7c
aaron.harpt <aaron.harpt@gmail.com>
2016-03-14 18:01:32 -0500

    added code for gulp-livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 // requiring gulp and gulp plugins
 var gulp = require("gulp");
 var plugins = require("gulp-load-plugins")({DEBUG: true});
-var clean = require("del");	
+var clean = require("del");
+var gulp-livereload = ("gulp-livereload");
+livereload({ start: true })
 
 // Concat javascript and put into a file named production.js in the js folder
 gulp.task('concatScripts', function() {
@@ -31,20 +33,18 @@ gulp.src("src/main.scss")
 .pipe(plugins.sourcemaps.write("./"))
 .pipe(gulp.dest("css"))
 // allows automatic css injecting
-.pipe(browsersync.stream());
+.pipe(livereload());
 });
 
 // watch task to watch for changes to css, js, html
 gulp.task("watch",["compileSass", "minifyScripts", "concatScripts"], function(){
 	// browsersync
-	browsersync.init({
-		server: "./"
-		});
+	livereload.listen();
 	// watch sass/js for changes
 	gulp.watch("src/main.scss", ["compileSass"]);
 	gulp.watch("js/production.js", ["concatScripts"]);
 	// watch html for changes to inject in browsersync
-	gulp.watch("./*.html").on("change", browsersync.reload);
+	gulp.watch("./*.html");
 });
 
 gulp.task("clean", function() {
--- js/production.js
+++ js/production.js
@@ -1,1 +1,5 @@
-var name="Aaron",nam=hello;document.write("heading");
+var name = "Aaron";
+var nam = hello;
+
+document.write("heading");
+//# sourceMappingURL=production.js.map
