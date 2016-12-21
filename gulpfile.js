3c9620a3fa2869951d99e4efc5102f2693efa636
aharpt <aaron.harpt@gmail.com>
2016-12-21 10:33:02 -0600

    Updated gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,68 @@
-// requiring gulp and gulp plugins
+// Requiring gulp and gulp plugins
 var gulp = require("gulp");
 var plugins = require("gulp-load-plugins")({DEBUG: true});
 
 
-// gulp webserver
-gulp.task("webserver", function() {
-	gulp.src("./")
-	.pipe(plugins.webserver({
-		livereload: true,
-		open: true,
-		fallback: "index.html"
-	}));
+// Gulp webserver
+gulp.task("webserver", function () {
+    "use strict";
+    gulp.src("./")
+        .pipe(plugins.webserver({
+            livereload: true,
+            open: true,
+            fallback: "index.html"
+        }));
 });
 
 
 // Concat javascript and put into a file named production.js in the js folder
-gulp.task('concatScripts', function() {
-	return gulp.src('src/*.js')
-	  .pipe(plugins.sourcemaps.init())
-.pipe(plugins.concat('production.js'))
-  .pipe(plugins.sourcemaps.write("./"))
-.pipe(gulp.dest('js'))
- .pipe(plugins.livereload());
+gulp.task("concatScripts", function () {
+    "use strict";
+    return gulp.src("src/*.js")
+        .pipe(plugins.sourcemaps.init())
+        .pipe(plugins.concat("production.js"))
+        .pipe(plugins.sourcemaps.write("./"))
+        .pipe(gulp.dest("js"))
+        .pipe(plugins.livereload());
 });
 
-// minify javascript and put into a keep it at production.js in the js folder
-gulp.task("minifyScripts", ["concatScripts"], function(){
-	return gulp.src('js/production.js')
-	.pipe(plugins.uglify())
-	 .pipe(gulp.dest("js"))
-	 .pipe(plugins.livereload());
+// Minify javascript and put into a keep it at production.js in the js folder
+gulp.task("minifyScripts", ["concatScripts"], function () {
+    "use strict";
+    return gulp.src("js/production.js")
+        .pipe(plugins.uglify())
+        .pipe(gulp.dest("js"))
+        .pipe(plugins.livereload());
 });
 
-//compile sass Automatically
- gulp.task("compileSass", function(){
- gulp.src("src/main.scss")
- .pipe(plugins.sourcemaps.init())
-//  autoprefixer to add prefixes for older browsers
- .pipe(plugins.autoprefixer())
- .pipe(plugins.sass())
- .pipe(plugins.sourcemaps.write("./"))
- .pipe(gulp.dest("css"))
-// // allows automatic css injecting
- .pipe(plugins.livereload());
- });
-
-// watch task to watch for changes to sass and js
-gulp.task("watch",["compileSass", "minifyScripts", "concatScripts"], function(){
-	plugins.livereload.listen();
-	// watch sass/js for changes
-	gulp.watch("src/*.scss", ["compileSass"]);
-	gulp.watch("src/*.js", ["concatScripts", "minifyScripts"]);
+// Compile sass automatically
+gulp.task("compileSass", function () {
+    "use strict";
+    gulp.src("src/main.scss")
+        .pipe(plugins.sourcemaps.init())
+        //  autoprefixer to add prefixes for older browsers
+        .pipe(plugins.autoprefixer())
+        .pipe(plugins.sass())
+        .pipe(plugins.sourcemaps.write("./"))
+        .pipe(gulp.dest("css"))
+        // allows automatic css injecting
+        .pipe(plugins.livereload());
 });
 
- gulp.task("build", ["webserver", "concatScripts", "minifyScripts", "compileSass", "watch"], function() {
+// Watch task to watch for changes to sass and js
+gulp.task("watch", ["compileSass", "minifyScripts", "concatScripts"], function () {
+    "use strict";
+    plugins.livereload.listen();
+    // Watch sass/js for changes
+    gulp.watch("src/*.scss", ["compileSass"]);
+    gulp.watch("src/*.js", ["concatScripts", "minifyScripts"]);
+});
 
- });
+gulp.task("build", ["webserver", "concatScripts", "minifyScripts", "compileSass", "watch"], function () {
+    "use strict";
+});
 
-gulp.task("default", function() {
-	gulp.start("build");
+gulp.task("default", function () {
+    "use strict";
+    gulp.start("build");
 });
